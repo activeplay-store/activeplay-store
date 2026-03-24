48381f68cfc5b62427498c1277edec4bdd67abb5
Sergey <@email.com>
2026-03-24 03:47:44 +0300

    fix: пути в ecosystem.config

--- agent/ecosystem.config.js
+++ agent/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'ap-agent',
     script: 'src/index.js',
-    cwd: '/home/activeplay/activeplay-store/agent',
+    cwd: '/var/www/activeplay-store/agent',
     env: {
       NODE_ENV: 'production',
       TZ: 'Europe/Moscow'
@@ -10,8 +10,8 @@ module.exports = {
     autorestart: true,
     max_restarts: 10,
     restart_delay: 5000,
-    error_file: '/home/activeplay/.pm2/logs/ap-agent-error.log',
-    out_file: '/home/activeplay/.pm2/logs/ap-agent-out.log',
+    error_file: '/root/.pm2/logs/ap-agent-error.log',
+    out_file: '/root/.pm2/logs/ap-agent-out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
   }]
 };
